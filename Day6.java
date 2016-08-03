9cdadb4d967715cc6e6e7b4a5996371b0e49737d
Anish Joshi <anishjoshi500@rocketmail.com>
2016-08-03 11:37:58 -0400

    Day 6: Let's Review

--- Day6.java
+++ Day6.java
@@ -1,1 +1,30 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+        String s;
+        
+        for(int i=0;i<n;i++)
+        {
+        s=sc.next();
+        char c[]=s.toCharArray();
+        for(int j=0;j<c.length;j=j+2)
+        {
+            System.out.print(c[j]);
+        }
+        System.out.print(" ");
+            for(int k=1;k<c.length;k=k+2)
+        {
+            System.out.print(c[k]);
+        }
+        System.out.println();
+        }
+    }
+}
