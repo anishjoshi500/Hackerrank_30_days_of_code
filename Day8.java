32fb7724800debe240c866ccc3ec54081e6ea316
Anish Joshi <anishjoshi500@rocketmail.com>
2016-08-03 11:40:18 -0400

    Day 8: Dictionaries and Maps

--- Day8.java
+++ Day8.java
@@ -1,1 +1,28 @@
+import java.util.*;
+import java.io.*;
 
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        HashMap<String, Integer> PhoneBook= new HashMap<String, Integer>();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            PhoneBook.put(name,phone);
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            if(PhoneBook.containsKey(s))
+            {
+                int value = PhoneBook.get(s);
+                System.out.println(s +"="+ value);
+            }
+            else
+            {
+                System.out.println("Not found");
+            }
+        }
+        in.close();
+    }
+}
