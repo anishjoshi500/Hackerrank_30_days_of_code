ff1bc3472e698ce6303c65d6b64599a7c8a13105
Anish Joshi <anishjoshi500@rocketmail.com>
2016-08-03 11:53:33 -0400

    Day 15: Linked List

--- Day15.java
+++ Day15.java
@@ -1,1 +1,17 @@
+public static  Node insert(Node head,int data)
+	{
+    Node nod = new Node(data);
 
+        if (head == null) {
+            head = nod;
+            return head;
+        }
+
+        Node t = head;
+        while (t.next != null) {
+            t = t.next;
+        }
+
+        t.next = nod;
+        return head;
+    }
