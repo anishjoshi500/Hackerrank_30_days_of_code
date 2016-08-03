87bee8f74cd0f9abebb1bc9dffe5105b81a2baa3
Anish Joshi <anishjoshi500@rocketmail.com>
2016-08-03 11:50:36 -0400

    Day 13: Abstract Classes

--- Day13.java
+++ Day13.java
@@ -1,1 +1,20 @@
-
+class MyBook extends Book
+    {
+    //public String title;
+    //public String author;
+    private int price;
+    public MyBook(String title, String author, int price)
+        {
+        super(title, author);
+        this.title=title;
+        this.author=author;
+        this.price=price;
+    }
+    void display()
+        {
+        System.out.println("Title: "+title);
+        System.out.println("Author: "+author);
+        System.out.println("Price: "+price);
+    }
+    
+}
