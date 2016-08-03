ddba742824fd2bf9d959c3e63258356103b528e7
Anish Joshi <anishjoshi500@rocketmail.com>
2016-08-03 12:00:23 -0400

    Day 18: Queues and Stacks

--- Day18.java
+++ Day18.java
@@ -1,1 +1,19 @@
-
+public class Solution {
+        Queue<Character> q = new LinkedList<>();
+        Deque<Character> s = new ArrayDeque<>();
+	    
+	    public void enqueueCharacter(char ch) {
+	        q.add(ch);
+	    }
+	    public char dequeueCharacter() {
+	        return q.remove();
+	    }
+        
+        
+        public void pushCharacter(char ch) {
+	        s.push(ch);
+	    }
+	    
+	    public char popCharacter() {
+	        return s.pop();
+	    }
