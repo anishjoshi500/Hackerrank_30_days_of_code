a63ed668d1ebceab7d7d3ac2fcfa07e698a0a9f1
Anish Joshi <anishjoshi500@rocketmail.com>
2016-08-03 11:27:52 -0400

    Day 2: Operators

--- Day3.java
+++ Day3.java
@@ -1,1 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Arithmetic {
 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double mealCost = scan.nextDouble(); 
+        int tipPercent = scan.nextInt(); 
+        int taxPercent = scan.nextInt();
+        scan.close();
+        double taxer=(taxPercent/100.0)*(mealCost);
+        double tipper=(tipPercent/100.0)*(mealCost);
+        double summer= mealCost+taxer+tipper;
+        int totalCost = (int) Math.round(summer);
+        System.out.println("The total meal cost is "+totalCost+" dollars.");
+    }
+}
