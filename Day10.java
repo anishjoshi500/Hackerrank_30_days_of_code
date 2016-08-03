a92e89af8ef09891f089ba309e68190dd0bd4ffc
Anish Joshi <anishjoshi500@rocketmail.com>
2016-08-03 11:44:31 -0400

    Day 10: Binary Numbers

--- Day10.java
+++ Day10.java
@@ -1,1 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        String b=Integer.toBinaryString(n);
+        String[] s= b.split("0");
+        Arrays.sort(s);
+        int x = s[s.length - 1].length();
+        System.out.println(x);
+    }
+}
