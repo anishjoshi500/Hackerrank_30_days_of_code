1c64f1bee79d152ebf8fb1583ebc69e83ad07a64
Anish Joshi <anishjoshi500@rocketmail.com>
2016-08-03 12:07:35 -0400

    Day 23: BST Level-Order Traversal

--- Day23.java
+++ Day23.java
@@ -1,1 +1,17 @@
+static void levelOrder(Node root){
+      Queue<Node> queue = new LinkedList<Node>();
+		queue.add(root);
+		while (!queue.isEmpty()) {
+			Node h = queue.poll();
 
+			if (h == null) {
+				continue;
+			}
+
+			System.out.print(h.data + " ");
+
+			queue.add(h.left);
+			queue.add(h.right);
+		}
+		System.out.println();
+    }
