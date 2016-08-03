dce0b54f9edd5c2fa2349b225b00222b31396b3d
Anish Joshi <anishjoshi500@rocketmail.com>
2016-08-03 11:26:37 -0400

    Day 1: Data Types

--- Day2.java
+++ Day2.java
@@ -1,1 +1,14 @@
-
+int n=scan.nextInt();
+double n1=scan.nextDouble();
+String n2=scan.next();
+String n3=scan.nextLine();
+int sum=0;
+sum=i+n;
+System.out.println(sum);
+double sum1=0;
+sum1=d+n1;
+System.out.println(sum1);
+String h=n2.concat(n3);
+String p=s.concat(h);
+System.out.println(p);
+   
