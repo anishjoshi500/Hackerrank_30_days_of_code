03d3a07f0b0e2a65216c4fb8c1a357737d3c6dfa
Anish Joshi <anishjoshi500@rocketmail.com>
2016-08-03 12:12:22 -0400

    Day 26: Nested Logic

--- Day26.java
+++ Day26.java
@@ -7,7 +7,6 @@ import java.util.regex.*;
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc=new Scanner(System.in);
         int ard[]=new int[3];
         int erd[]=new int[3];
