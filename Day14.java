2430939d4a59b50cd74e8f832867b5452f3d6699
Anish Joshi <anishjoshi500@rocketmail.com>
2016-08-03 11:51:46 -0400

    Day 14: Scope

--- Day14.java
+++ Day14.java
@@ -1,1 +1,24 @@
-
+public Difference(int[] elements)
+    {
+        this.elements=elements;
+    }
+    public void computeDifference()
+    {
+       int maxarray[]=new int[(elements.length*elements.length)];
+       int u=0;
+       for(int r=0;r<(elements.length*elements.length);r++)
+       {
+           maxarray[r]=0;
+       } 
+       for(int i=0;i<elements.length;i++)
+       {
+       for(int j=0;j<elements.length;j++)
+       {
+        int difference=elements[i]-elements[j];
+        int actualDifference=Math.abs(difference);
+        maxarray[u++]=actualDifference;
+       }
+       }
+       Arrays.sort(maxarray);
+       maximumDifference=maxarray[u-1];
+    }
