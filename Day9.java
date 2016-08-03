e669e7354929385c0793f459baec05c3abfa084f
Anish Joshi <anishjoshi500@rocketmail.com>
2016-08-03 11:42:58 -0400

    Day 9: Recursion

--- Day9.java
+++ Day9.java
@@ -1,1 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+public class Solution {
+    public int factorial(int n)
+        {
+        if(n==0||n==1)
+        return 1;
+        else
+        return(n*(factorial(n-1)));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        Solution s=new Solution();
+        int fact=s.factorial(n);
+        System.out.println(fact);
+    }
+}
