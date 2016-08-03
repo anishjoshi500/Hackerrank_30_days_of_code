3d05b462ab4c2dfca2f648a34cf563da4a25f1c0
Anish Joshi <anishjoshi500@rocketmail.com>
2016-08-03 12:08:28 -0400

    Day 24: More Linked Lists

--- Day24.java
+++ Day24.java
@@ -1,1 +1,15 @@
-
+public static Node removeDuplicates(Node head) {
+      Node chead = head;
+	  if (head == null)
+	  return head;
+        Node nexter;
+		while (chead.next != null) {
+			if (chead.data == chead.next.data) {
+			    nexter = chead.next.next;
+				//chead.next = null;
+				chead.next = nexter;
+			} else 
+				chead = chead.next;
+		}
+		return head;
+    }
